dbb4422df611099c2a5a549140663c63c5c5542f
webzwo0i <webzwo0i@c3d2.de>
2020-07-21 02:12:19 +0200

    Revert "change_user_name, change_user_color"
    This reverts commit 4db0446d07663933876c1eed41b942c7ad06ed16.

--- tests/frontend/specs/change_user_color.js
+++ tests/frontend/specs/change_user_color.js
@@ -1,4 +1,4 @@
-describe("change user color", function(){
+xdescribe("change user color", function(){
   //create a new pad before each test run
   beforeEach(function(cb){
     helper.newPad(cb);
--- tests/frontend/specs/change_user_name.js
+++ tests/frontend/specs/change_user_name.js
@@ -1,4 +1,4 @@
-describe("change username value", function(){
+xdescribe("change username value", function(){
   //create a new pad before each test run
   beforeEach(function(cb){
     helper.newPad(cb);
