c9be279158812f8a544f68df270a04c1e44f95ac
webzwo0i <webzwo0i@c3d2.de>
2020-07-21 02:20:44 +0200

    Revert "delete,authorship_of_editions"
    This reverts commit ec319eb854463ae1e69fed228a8d63a976010fd8.

--- tests/frontend/specs/authorship_of_editions.js
+++ tests/frontend/specs/authorship_of_editions.js
@@ -1,4 +1,4 @@
-describe('author of pad edition', function() {
+xdescribe('author of pad edition', function() {
   var REGULAR_LINE = 0;
   var LINE_WITH_ORDERED_LIST = 1;
   var LINE_WITH_UNORDERED_LIST = 2;
--- tests/frontend/specs/delete.js
+++ tests/frontend/specs/delete.js
@@ -1,4 +1,4 @@
-describe("delete keystroke", function(){
+xdescribe("delete keystroke", function(){
   //create a new pad before each test run
   beforeEach(function(cb){
     helper.newPad(cb);
